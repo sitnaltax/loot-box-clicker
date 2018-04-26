1b41716d4930493416badbb8f718d8d3dfdbeeb1
unknown <sitnaltax@gmail.com>
2018-04-25 21:33:12 -0400

    toned down critical hits

--- src/app/hero/hero.service.ts
+++ src/app/hero/hero.service.ts
@@ -12,7 +12,7 @@ export class HeroService {
     hero: IHero;
     
     constructor(private _cashService: CashService, private _equipmentService: EquipmentService) {
-        this.hero = {name: "Rob", job: "Demi-Druid", power: 1, criticalChance: 0.05, criticalPower: 10};
+        this.hero = {name: "Rob", job: "Demi-Druid", power: 1, criticalChance: 0.04, criticalPower: 5};
      }
     
     getHero(): IHero {
@@ -21,7 +21,7 @@ export class HeroService {
 
     recalculatePower() {
         this.hero.power = this._equipmentService.calculatePower();
-        this.hero.criticalChance = 0.05;
-        this.hero.criticalPower = 10;
+        this.hero.criticalChance = 0.04;
+        this.hero.criticalPower = 5;
     }
 }
