591bd4bf219259c8b660d0ac271cfcdf9d7e098e
unknown <sitnaltax@gmail.com>
2018-05-14 23:03:21 -0400

    Added a checkmark for visual confirmation when you save. Resolves #35.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
 import { Component } from '@angular/core';
 import { StorageService } from './storage/storage.service';
+import { AnimationEvent, trigger, state, style, animate, transition, sequence } from '@angular/animations';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    animations: [
+        trigger('checkState', [
+            state('show', style({
+                opacity:1,
+            })),
+            state('hide', style({
+                opacity:0,
+            })),
+            transition('show => hide', animate('1500ms')),
+            transition('hide => show', animate('0ms'))
+        ])]
 })
 export class AppComponent {
     title = 'Loot Box Clicker';
+    checkState = "hide";
 
     constructor(private _storageService: StorageService) {
     }
 
     save() {
         this._storageService.triggerStore();
+        this.checkState = "show";
+        window.setTimeout(() => this.checkState = "hide", 200);
     }
 }
