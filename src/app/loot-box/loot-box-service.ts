d8cfa3709d5d80f584a17b703a3978485819ca18
Mike Binns <mike@mikebinns.net>
2018-05-30 13:18:40 -0400

    Trigger loot box opening if any exist on app start. Fixes #61.

--- src/app/loot-box/loot-box-service.ts
+++ src/app/loot-box/loot-box-service.ts
@@ -11,7 +11,6 @@ import { skillId } from '../trainer/skill';
 
 @Injectable()
 export class LootBoxService {
-
     lootBoxList: IShopItem[];
     currentlyOpeningBox: IShopItem;
     lootBoxOpeningTime: number;
@@ -21,15 +20,14 @@ export class LootBoxService {
 
     constructor(private _shopService: ShopService, private _lootService: LootService, private _storageService: StorageService,
         private _inventoryService: InventoryService, private _trainerService: TrainerService) {
-        if (this._storageService.retrieve("lootBoxes")) {
-            this.lootBoxList = this._storageService.retrieve("lootBoxes");
-        }
-        else {
+        if (this._storageService.retrieve('lootBoxes')) {
+            this.lootBoxList = this._storageService.retrieve('lootBoxes');
+        } else {
             this.lootBoxList = [];
         }
 
         this._storageService.autoSaveNotification.subscribe((dummy) => {
-            this._storageService.store("lootBoxes", this.lootBoxList);
+            this._storageService.store('lootBoxes', this.lootBoxList);
         });
 
         this._storageService.resetNotification.subscribe((dummy) => {
@@ -41,6 +39,7 @@ export class LootBoxService {
         this.lootBoxOpeningTime = 2500;
         this.progressSubject = new Subject<string>();
         this.progressNotification = this.progressSubject.asObservable();
+        this.openBox();
     }
 
     addLootBox(lootBox: IShopItem) {
@@ -57,13 +56,13 @@ export class LootBoxService {
     }
 
     openBox() {
-        if (this.lootBoxList.length == 0) {
+        if (this.lootBoxList.length === 0) {
             return;
         }
         if (this.currentlyOpeningBox != null) {
             return;
         }
-        this.progressSubject.next("box");
+        this.progressSubject.next('box');
         this.currentlyOpeningBox = this.lootBoxList.pop();
         this.possiblyOpenBonusBoxes();
         window.setTimeout(() => {
@@ -88,7 +87,7 @@ export class LootBoxService {
             if (Math.random() > this._trainerService.getRanksForSkillById(skillId.advancedAvarice) * 0.07) {
                 return;
             }
-            let bonusBox: IShopItem = this.lootBoxList.pop();
+            const bonusBox: IShopItem = this.lootBoxList.pop();
             this._lootService.getItemsForLootBox(bonusBox).forEach(item => this._inventoryService.addToInventory(item));
         }
     }
